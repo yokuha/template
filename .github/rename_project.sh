151bbe06bd05875503acf2cb6e36c6320d579a8d
yokuha <yokuha@users.noreply.github.com>
2023-08-31 12:50:15 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="yokuha"
+original_name="template"
+original_urlname="template"
+original_description="Awesome template created by yokuha"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
